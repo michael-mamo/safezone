f9cab9512517db4b4fc4e41ae9e6178817e3c02e
Michael <mikimamo921@gmail.com>
2022-08-23 14:46:08 +0300

    user ad

--- user-ad.php
+++ user-ad.php
@@ -221,12 +221,11 @@ li a {
                 <div class="col-lg-8 col-lg-offst-2" style="padding-left: 50px; margin-top: -40px;">
         				<h1>ማስታወቂያ</h1>
 
-           <h5>ማስታወቂያ</h5>
                 <table width="700" border="2" cellpadding="2" cellspacing="2">
                   <tr bgcolor="#2ECCFA">
                     <th>ማስታወቂያ ርዕስ</th>
                     <th>ማስታወቂያ</th>
-                     <th>የተመዘገበበት ቀን</th>
+                     <th>የማስታወቂያው ቀን</th>
                   </tr>
                   <?php 
                 $query = "SELECT *FROM  ad";
